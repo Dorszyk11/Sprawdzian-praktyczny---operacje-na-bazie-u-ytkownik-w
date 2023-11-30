9f064b1951d9012388bc076d8f73a8a3b0634006
Dorszyk11 <tymbeixpoi@gmail.com>
2023-11-30 11:19:30 +0100

    2

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-from flask import *
+from flask import Flask, jsonify
+app = Flask(__name__)
 
+users = []
+next_id = 1
+@app.route('/users', methods=['GET'])
+def get_users():
+    return jsonify(users), 200
