015545b553ed9786ea013e619286ae85e4048f4d
Dorszyk11 <tymbeixpoi@gmail.com>
2023-11-30 11:46:34 +0100

    3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 users = []
@@ -6,3 +6,15 @@ next_id = 1
 @app.route('/users', methods=['GET'])
 def get_users():
     return jsonify(users), 200
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = next((u for u in users if u['id'] == user_id), None)
+    return (jsonify(user), 200) if user else (jsonify({'error': 'Not found'}), 404)
+@app.route('/users', methods=['POST'])
+def create_user():
+    global next_id
+    data = request.json
+    data['id'] = next_id
+    next_id += 1
+    users.append(data)
+    return jsonify(data), 201
